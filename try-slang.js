4e3986489928890183757884e279d3a1037bcd2f
kaizhangNV <kazhang@nvidia.com>
2024-10-14 13:26:13 -0700

    Make it working on Linux with WebGPU setup

--- try-slang.js
+++ try-slang.js
@@ -4,6 +4,66 @@ var Slang;
 var globalSlangSession;
 var slangSession;
 
+async function webgpuInit(shaderCode)
+{
+    const adapter = await navigator.gpu?.requestAdapter();
+    const device = await adapter?.requestDevice();
+    if (!device)
+    {
+        fail('need a browser that supports WebGPU');
+        return;
+    }
+
+    const module = device.createShaderModule({code:shaderCode});
+    const pipeline = device.createComputePipeline({
+        label: 'compute pipeline',
+        layout: 'auto',
+        compute: {module},
+        });
+
+    let usage = GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC;
+    const workgroupSize = [4, 1, 1];
+    const dispatchCount = [1, 1, 1];
+    const numberElements = workgroupSize.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+    const size = numberElements * 4; // int type
+    const outputBuffer = device.createBuffer({size, usage});
+
+    usage = GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST;
+    const outputBufferRead = device.createBuffer({size, usage});
+
+    const bindGroup = device.createBindGroup({
+        layout: pipeline.getBindGroupLayout(0),
+        entries: [
+                { binding: 0, resource: { buffer: outputBuffer }},
+                ],
+        });
+
+    // Encode commands to do the computation
+    const encoder = device.createCommandEncoder({ label: 'compute builtin encoder' });
+    const pass = encoder.beginComputePass({ label: 'compute builtin pass' });
+
+    pass.setPipeline(pipeline);
+    pass.setBindGroup(0, bindGroup);
+    pass.dispatchWorkgroups(...dispatchCount);
+    pass.end();
+
+    encoder.copyBufferToBuffer(outputBuffer, 0, outputBufferRead, 0, size);
+
+    // Finish encoding and submit the commands
+    const commandBuffer = encoder.finish();
+    device.queue.submit([commandBuffer]);
+
+    // Read the results
+    await Promise.all([
+        outputBufferRead.mapAsync(GPUMapMode.READ),
+    ]);
+
+    const output = new Int32Array(outputBufferRead.getMappedRange());
+
+    outputResult(output)
+}
+
+
 var TrySlang = {
     iterate: function() {
 
