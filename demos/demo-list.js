13fa403d051e648ddeb7f87df39a7dc9eb45f2de
Sai Praveen Bangaru <sbangaru@mit.edu>
2024-11-07 19:58:05 -0500

    Add 2D forward splatting demo

--- demos/demo-list.js
+++ demos/demo-list.js
@@ -3,5 +3,6 @@ var demoList = [
   { name: "Simple Image", url: "simple-image.slang" },
   { name: "-", url: "" },
   { name: "ShaderToy: Circle", url: "circle.slang" },
-  { name: "ShaderToy: Ocean", url: "ocean.slang" }
+  { name: "ShaderToy: Ocean", url: "ocean.slang" },
+  { name: "2D Splat Rasterizer", url: "gsplat2d.slang" },
 ];
