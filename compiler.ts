34aedf22fc0cf6e0acd100d245c0e71d60508d66
Devon7925 <devonrutledge03@gmail.com>
2025-01-16 21:34:39 -0700

    Validate Resource commands and properly account for element size

--- compiler.ts
+++ compiler.ts
@@ -73,21 +73,54 @@ type BindingDescriptor = {
 
 export type Bindings = Map<string, GPUBindGroupLayoutEntry>;
 
+export type ReflectionBinding = {
+    "kind": "uniform",
+    "offset": number,
+    "size": number,
+} | {
+    "kind": "descriptorTableSlot",
+    "index": number,
+};
+
+export type ReflectionType = {
+    "kind": "struct",
+    "name": string,
+    "fields": ReflectionParameter[]
+} | {
+    "kind": "vector",
+    "elementCount": number,
+    "elementType": ReflectionType,
+} | {
+    "kind": "scalar",
+    "scalarType": `${"uint" | "int"}${8 | 16 | 32 | 64}` | `${"float"}${16 | 32 | 64}`,
+} | {
+    "kind": "resource",
+    "baseShape": "structuredBuffer",
+    "access"?: "readWrite",
+    "resultType": ReflectionType
+} | {
+    "kind": "resource",
+    "baseShape": "texture2D",
+    "access"?: "readWrite"
+};
+
+export type ReflectionParameter =  {
+    "binding": ReflectionBinding,
+    "name": string,
+    "type": ReflectionType,
+    "userAttribs"?: {
+        "arguments": any[],
+        "name": string,
+    }[],
+}
+
 export type ReflectionJSON = {
     "entryPoints": {
         "name": string,
         "semanticName": string,
         "type": unknown
     }[],
-    "parameters": {
-        "binding": unknown,
-        "name": string,
-        "type": unknown,
-        "userAttribs"?: {
-            "arguments": any[],
-            "name": string,
-        }[],
-    }[],
+    "parameters": ReflectionParameter[],
 };
 
 
