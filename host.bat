56d16ca661152d0483bf9804607cf14493913a4d
Devon <devonrutledge03@gmail.com>
2025-02-02 14:44:25 -0700

    Vue rewrite (#106)
    * vue rewrite
    
    * Improve tooltip code
    
    * Fix compilation visuals and debug overlay overflow
    
    * Fix error reporting
    
    * Working deploy and networking improvements
    
    * CI and README updates
    
    * readme tuning and ci check
    
    * Hopefully final fixes
    
    * fix pages build paths
    
    * Hopefully fix pages build again
    
    * Remove unused dependency
    
    * Fix reflection view not being scrollable
    
    * Fixed inconsistent loading in dev environment
    
    * Ensure tooltips can overflow

--- host.bat
+++
@@ -1,4 +0,0 @@
-set TRY_SLANG_SOURCE_DIRECTORY_PATH=D:\git_repo\slang-playground
-set TRY_SLANG_TARGET_DIRECTORY_PATH=D:\git_repo\slang-playground
-set PATH=%PATH%;C:\Ruby33-x64\msys64\usr\bin
-python -m http.server 8000
