154831bd8a7bdb5c0a1f9f44b4d6e22464ffbce5
Jay Kwak <82421531+jkwak-work@users.noreply.github.com>
2025-05-03 04:35:53 +0000

    Add smoke test and CI yaml (#141)
    Adding CI yaml.
    Adding smoke-test.js
    Improve the build scripts

--- spirv-tool-wasm-build.sh
+++ spirv-tool-wasm-build.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-if [ ! -d emsdk ]; then
+if [ ! -d emsdk ]
+then
     git clone https://github.com/emscripten-core/emsdk.git emsdk
 fi
 
 pushd emsdk
-
-./emsdk install latest
-./emsdk activate latest
-source ./emsdk_env.sh
-
+	sed -i 's/\r$//' emsdk emsdk_env.sh
+	/bin/sh ./emsdk install latest
+	/bin/sh ./emsdk activate latest
+	source ./emsdk_env.sh
 popd
+
+if [ ! -d spirv-tools ]
+then
+	git clone https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
+fi
 
 pushd spirv-tools
 git checkout vulkan-sdk-1.3.290.0
 
 python3 utils/git-sync-deps
 
-sed -i '52i        -s EXPORT_ES6 \\' ./source/wasm/build.sh
-echo $(cat ./source/wasm/build.sh)
-./source/wasm/build.sh
+# add an additional option to emcc command
+sed -i 's/\r$//' source/wasm/build.sh
+sed -i 's/-s MODULARIZE \\/-s MODULARIZE -s EXPORT_ES6 \\/' source/wasm/build.sh
+
+bash -x source/wasm/build.sh
 
 cp out/web/spirv-tools.wasm ../
 cp out/web/spirv-tools.js ../
